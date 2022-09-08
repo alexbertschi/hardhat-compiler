3dfbeca18393ac5b2564d475699dd4596cff4f49
alexbertschi <alex.bertschi@gmail.com>
2022-09-08 19:45:46 +0200

    Complete Project with successful contract deployment. However, contract verification (etherscan) fails

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_API,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API
+    },
+    customChains: [
+      {
+        network: "goerli",
+        chainId: 5,
+        urls: {
+          apiURL: "https://api-goerli.etherscan.io/api",
+          browserURL: "https://goerli.etherscan.io"
+        }
+      }
+    ]
+  }
+}
